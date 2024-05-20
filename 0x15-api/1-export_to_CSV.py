8646e25c5ace94ce9d31ad6dbbea026941478c1c
ItumelengRandyMalau <itumelengrandy5@gmail.com>
2024-05-20 12:18:06 -0700

    1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Returns information about an employee with a given ID and exports data to CSV"""
+
+import csv
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    # Get the employee ID from command line arguments
+    employee_id = int(argv[1])
+
+    # Fetch TODO list data from the API
+    resp = requests.get('https://jsonplaceholder.typicode.com/todos/')
+    json_repr = resp.json()
+
+    # Fetch employee name from the API using employee ID
+    name_resp = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
+    name = name_resp.json().get('username')
+
+    # Open CSV file to write data
+    with open('{}.csv'.format(employee_id), 'w') as csv_data:
+        # Create CSV writer with specified delimiter and quoting style
+        csv_writer = csv.writer(
+            csv_data,
+            delimiter=",",
+            quotechar='"',
+            quoting=csv.QUOTE_ALL
+            )
+
+        # Iterate through tasks and write data to CSV file
+        for task in json_repr:
+            # Check if the task belongs to the specified employee
+            if task.get('userId') == employee_id:
+                # Write task information to CSV file
+                csv_writer.writerow(
+                    [
+                        task.get('userId'),
+                        name,
+                        task.get('completed'),
+                        task.get('title')
+                    ]
+                )
+
